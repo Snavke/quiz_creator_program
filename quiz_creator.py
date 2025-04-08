fa3be01d77124879ad5b0ce48bc6c9d8303d2628
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-04-08 16:17:14 +0800

    Fixed Looping bug. Resorted to a single dict for efficiency. Commit 5

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,7 +1,5 @@
 # dictionary to store inputs
-dict_questions = {}
-dict_choices = {}
-dict_answer = {}
+dict_quiz = {}
 
 # User provides questions
 while True:
@@ -9,23 +7,25 @@ while True:
     choices_input = input("Enter 4 Possible Choices: ")
     answer_input = input("Enter Answer (The Letter): ")
 
-    dict_questions = {"Question": question_input}
-    dict_choices = {"Choices": choices_input}
-    dict_answer ={"Answer": answer_input}
+    dict_question = {"Question": question_input,
+                     "Choices": choices_input,
+                     "Answer": answer_input
+                     }
+    
 
-    exit_prompt = input("Continue adding question? (yes/no): ").lower
+    exit_prompt = input("Continue adding question? (yes/no): ").lower()
 
     if exit_prompt != "yes":
+        print ("Quiz Summary: ")
         break
 
-print (dict_questions, dict_choices, dict_answer)
+print (dict_question)
 # User provides possible choices
 
 # User provides correct answer from possible choices
 
 
 
-print ()
 # Confirm questions, choice, and answers, give option to redo
 
 # Continue asking until user chooses to terminate program
