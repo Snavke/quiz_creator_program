e2b731f4aedb20b95342a32bb21d8ae08eb86ce5
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-04-08 12:36:02 +0800

    Updated Pseudo Code to better align with vision. Commit 2

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,6 +4,10 @@
 
 # User provides correct answer from possible choices
 
+# Confirm questions, choice, and answers, give option to redo
+
 # Continue asking until user chooses to terminate program
 
-# Store collected data per user "session" 
+# Add functionality to choose question to delete, or to edit (will overwrite)
+
+# Store collected data per user "session" to text file
