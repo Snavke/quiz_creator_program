c64139c3b4c0e09bd60fa933b7f6df22e7a56baf
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-04-09 00:52:31 +0800

    Added a way to edit existing questions before terminating program. Commit 8

--- quiz_creator.py
+++ quiz_creator.py
@@ -9,12 +9,12 @@ while True:
 # Asks choices
     choice_labels = ['A', 'B', 'C', 'D']
     choices_dict = {}
-    print ("Enter the 4 Choices: ")
+    print ("Enter the 4 choices: ")
     for label in choice_labels:
         choice_text = input(f"Choice {label}: ")
         choices_dict[label] = choice_text
 # Asks answer
-    answer_input = input("Enter Answer (The Letter): ")
+    answer_input = input("Enter Answer (The Letter): ").upper
 
 # Store data to dictionary
     dict_quiz[f"Question {question_number}"] = {
@@ -25,8 +25,38 @@ while True:
     
     question_number += 1
 
-# Confirm questions, choice, and answers, give option to redo
-    exit_prompt = input("Continue adding question? (yes/no): ").lower()
+# Confirm questions, choice, and answers, give option to redo & Add functionality to choose question to delete, or to edit (will overwrite)
+    exit_prompt = input("Continue adding question? (yes/edit/delete/exit): ").lower()
+
+    if exit_prompt == "edit":
+        print ("\n Questions:")
+        for items in dict_quiz:
+            print (items)
+
+        to_edit = input("Please enter the Question number to edit (number only): ")
+        key = f"Question {to_edit}"
+        if key in dict_quiz:
+            print (f"\nEditing {key}")
+            new_question = input ("Enter new question: ")
+
+            print("Enter the new 4 choices: ")
+            new_choices = {}
+            for label in choice_labels:
+                new_choice = input(f"Choice {label}: ")
+                new_choices[label] = new_choice
+
+            new_answer = input("Enter new correct answer (The letter): ").upper()
+# Overwrites over the existing dictionary entry
+            dict_quiz[key] = {
+                "Question": new_question,
+                "Choices": new_choices,
+                "Answer": new_answer
+            }
+        else: 
+            print ("Invalid question number.")
+        
+
+
 # Program terminates if yes
     if exit_prompt != "yes":
         print ("\nQuiz Summary: ")
@@ -39,11 +69,5 @@ while True:
 
 
 
-
-
-
-
-
-# Add functionality to choose question to delete, or to edit (will overwrite)
 
 # Store collected data per user "session" to text file
