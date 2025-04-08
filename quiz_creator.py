8566cf1b593b337f0add1bf942fc83dd6b603f54
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-04-08 12:06:05 +0800

    Pseudo Code Draft, 1st Commit

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# User provides questions
+
+# User provides possible choices
+
+# User provides correct answer from possible choices
+
+# Continue asking until user chooses to terminate program
+
+# Store collected data per user "session" 
