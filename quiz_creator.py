b2da4637514845169e2ae0bfbac77d4dc666c9ae
Altheo Mananquil <altheo.mananquil@gmail.com>
2025-04-08 14:25:24 +0800

    Appending to list works. Commit 3

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,9 +1,23 @@
+# list to store inputs
+list_questions = []
+list_choices = []
+list_answer = []
+
 # User provides questions
 
+question_input = input("Enter Question: ")
+list_questions.append(question_input) 
+
 # User provides possible choices
+choices_input = input("Enter 4 Possible Choices: ")
+list_choices.append(choices_input)
 
 # User provides correct answer from possible choices
+answer_input = input("Enter Answer (The Letter): ")
+list_answer.append(answer_input)
 
+
+print (list_questions, list_choices, list_answer)
 # Confirm questions, choice, and answers, give option to redo
 
 # Continue asking until user chooses to terminate program
